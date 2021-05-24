1881fd01438be6447c8031f5d18d604dfcea8167
Tobias Büschel <tobias.buschel.15@alumni.ucl.ac.uk>
2021-05-24 14:14:37 +0800

    build: add cypress, eslint setup and test dependencies

---
+++ .releaserc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        message: 'chore: release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+}
