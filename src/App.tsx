ac6299ade813572713d091ccfaf0a8fdb66a54b1
Tobias Büschel <13087421+tobiasbueschel@users.noreply.github.com>
2021-06-03 19:09:16 +0800

    feat: add loading spinner to app (#21)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import { BrowserRouter as Router, Redirect, Switch } from 'react-router-dom'
+import { Box, CircularProgress } from '@material-ui/core'
 import { routes, ROUTE_PATHS } from './routes'
 import LayoutRoute from './layout/LayoutRoute'
 
 function App(): JSX.Element {
   return (
-    <React.Suspense fallback="TODO">
+    <React.Suspense
+      fallback={
+        <Box
+          height="100vh"
+          width="100vw"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <CircularProgress />
+        </Box>
+      }
+    >
       <Router>
         <Switch>
           {routes.map(({ exact, path, component, isPublic }) => (
