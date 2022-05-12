6cacd047de821d6c27dcc3120d2320e3258ade55
IOsonoTAN <im@tzv.me>
2022-05-12 10:24:16 +0700

    feat: create an interface for documents

--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -24,6 +24,7 @@ import {
   SupervisedUserCircle as AdminUsersIcon,
   LoyaltyOutlined as LoyaltyOutlinedIcon,
   GroupAdd as GroupAddIcon,
+  LibraryBooks as LibraryBooksIcon,
 } from '@material-ui/icons'
 import { ROUTE_PATHS } from 'routes'
 import { useTranslation } from 'react-i18next'
@@ -154,6 +155,18 @@ function Sidebar({ isOpen, onSidebarToggle }: SidebarProps): JSX.Element {
       {
         subHeader: t('sidebar.others'),
         allowedRoles: [ROLES.SUPER_ADMIN, ROLES.ADMIN],
+      },
+      {
+        title: t('sidebar.documents'),
+        path: ROUTE_PATHS.DOCUMENTS,
+        icon: <LibraryBooksIcon />,
+        allowedRoles: [
+          ROLES.SUPER_ADMIN,
+          ROLES.ADMIN,
+          ROLES.CUSTOMER_SUPPORT,
+          ROLES.OPERATION,
+          ROLES.MARKETING,
+        ],
       },
       {
         title: t('sidebar.chargingLocations'),
