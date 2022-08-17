0c876333eee63c0c8c473250f586cd7fa07b29fc
Krissada Boontrigratn <krissada@appsynth.net>
2022-08-17 12:15:59 +0700

    feat(EVME-728, EVME-803): integrate with the api

--- src/services/web-bff/car-activity.ts
+++ src/services/web-bff/car-activity.ts
@@ -1,22 +1,48 @@
 import { BaseApi } from 'api/baseApi'
 import {
   CarActivity,
+  CarActivityResponse,
   CarActivitySchedule,
   CarActivityCreateScheduleProps,
   CarActivityService,
+  CarActivityListProps,
 } from 'services/web-bff/car-activity.type'
 
 export const createSchedule = async (
   body: CarActivityCreateScheduleProps
 ): Promise<CarActivity> => {
   const response: CarActivity = await BaseApi.post('/v1/bookings/reservation', body)
-    .then((response) => response.data.data.data.booking)
+    .then(
+      ({
+        data: {
+          data: { booking },
+        },
+      }) => booking
+    )
     .catch((error) => {
       if (error.response) {
         throw error.response
       }
       throw error
     })
+
+  return response
+}
+
+export const getActivities = async (
+  params: CarActivityListProps
+): Promise<CarActivityResponse['data']> => {
+  const response: CarActivityResponse['data'] = await BaseApi.get('/v1/car-activities/search', {
+    params,
+  }).then((response) => response.data.data)
+
+  return response
+}
+
+export const getSchedulesByCarId = async (carId: string): Promise<CarActivitySchedule[]> => {
+  const response: CarActivitySchedule[] = await BaseApi.get(`/v1/bookings/carId/${carId}`).then(
+    (response) => response.data.data
+  )
 
   return response
 }
@@ -29,16 +55,9 @@ export const getServices = async (): Promise<CarActivityService[]> => {
   return response
 }
 
-export const getActivitiesByCarId = async (carId: string): Promise<CarActivitySchedule[]> => {
-  const response: CarActivitySchedule[] = await BaseApi.get(`/v1/bookings/carId/${carId}`).then(
-    (response) => response.data.data
-  )
-
-  return response
-}
-
 export default {
   createSchedule,
+  getActivities,
+  getSchedulesByCarId,
   getServices,
-  getActivitiesByCarId,
 }
--- src/services/web-bff/car-activity.type.ts
+++ src/services/web-bff/car-activity.type.ts
@@ -2,6 +2,22 @@ import { AdminUser } from 'services/web-bff/admin-user.type'
 import { Car } from 'services/web-bff/car.type'
 import { Response, ResponseWithPagination } from 'services/web-bff/response.type'
 
+export enum CarActivityBookingTypeIds {
+  RENT = 1,
+  PREVENTIVE_MAINTENANCE = 2,
+  RESERVED = 3,
+  PR = 4,
+  MARKETING = 5,
+  BREAKDOWN = 6,
+  REPAIR = 7,
+  EVME_INTERNAL = 8,
+  CARPOOL = 9,
+  B2B_DELIVERED = 10,
+  B2B_PENDING_DELIVERY = 11,
+  RED_PLATE = 12,
+  OTHERS = 13,
+}
+
 export interface CarActivity {
   brandName: string
   modelName: string
@@ -35,10 +51,21 @@ export interface CarActivityCreateScheduleProps {
 }
 
 export interface CarActivityService {
-  id: string
+  id: number
   nameEn: string
   nameTh: string
 }
+
+export interface CarActivityListProps {
+  page: number
+  size: number
+}
+
+export type CarActivityResponse = {
+  data: {
+    cars: CarActivity[]
+  }
+} & ResponseWithPagination
 
 export type CarActivityScheduleResponse = {
   data: {
