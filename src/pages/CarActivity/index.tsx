bf9f576228ecf93693cae584860d3739a5e9ee2e
Krissada Boontrigratn <krissada@appsynth.net>
2022-08-18 14:47:24 +0700

    fix(EVME-1475): handle nullable value

--- src/pages/CarActivity/index.tsx
+++ src/pages/CarActivity/index.tsx
@@ -176,6 +176,13 @@ export default function CarActivity(): JSX.Element {
     getActivities({ page, size: pageSize })
   )
 
+  const checkAndRenderValue = (value: string) => {
+    if (!value) {
+      return '-'
+    }
+    return value
+  }
+
   const carActivities =
     (carActivitiesData &&
       carActivitiesData.cars?.length > 0 &&
@@ -184,11 +191,13 @@ export default function CarActivity(): JSX.Element {
           <TableRow key={`car-activity-${carActivity.carId}`}>
             <TableCell className={classes.tableColumnCarInfo}>
               <Link to={`/car-activity/${carActivity.carId}`} className={classes.link}>
-                <div className={classes.textBold}>{carActivity.brandName}</div>
-                <div className={classes.textBold}>{carActivity.plateNumber}</div>
+                <div className={classes.textBold}>{checkAndRenderValue(carActivity.brandName)}</div>
+                <div className={classes.textBold}>
+                  {checkAndRenderValue(carActivity.plateNumber)}
+                </div>
                 <div className={classes.subText}>
-                  <div>{carActivity.modelName}</div>
-                  <div>{carActivity.color}</div>
+                  <div>{checkAndRenderValue(carActivity.modelName)}</div>
+                  <div>{checkAndRenderValue(carActivity.color)}</div>
                 </div>
               </Link>
             </TableCell>
