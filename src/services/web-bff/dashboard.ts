150b65501814af70754bd9606ff56bf937f24aed
Krissada Boontrigratn <krissada@appsynth.net>
2022-09-08 13:17:44 +0700

    refactor(EVME-1629): change api version

--- src/services/web-bff/dashboard.ts
+++ src/services/web-bff/dashboard.ts
@@ -2,7 +2,7 @@ import { BaseApi } from 'api/baseApi'
 import { DashboardResponse } from 'services/web-bff/dashboard.type'
 
 export const getInformations = async (): Promise<DashboardResponse> => {
-  const response: DashboardResponse = await BaseApi.get('v1/dashboard/summary').then(
+  const response: DashboardResponse = await BaseApi.get('/v2/dashboard/summary').then(
     (response) => response.data
   )
 
