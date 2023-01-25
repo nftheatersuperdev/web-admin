82881f552eeb131491dd8c08f263964391027dab
Krissada Boontrigratn <krissada@appsynth.net>
2023-01-25 11:12:03 +0700

    fix: remove unused code

--- src/services/web-bff/dashboard.ts
+++ src/services/web-bff/dashboard.ts
@@ -5,7 +5,6 @@ export const getInformations = async (): Promise<DashboardResponse> => {
   const response: DashboardResponse = await AdminBffAPI.get('/v1/dashboard/summary').then(
     (response) => response.data
   )
-  console.log('response ->', response)
 
   return response
 }
