4ffe0fce34e0cc48e6467a704eebcfb8153b2761
Krissada Boontrigratn <krissada@appsynth.net>
2023-02-06 14:24:05 +0700

    refactor: remove unused code

--- src/services/web-bff/subscription.ts
+++ src/services/web-bff/subscription.ts
@@ -1,12 +1,8 @@
 import { AdminBffAPI } from 'api/admin-bff'
-import { BaseApi } from 'api/baseApi'
-import axios from 'axios'
 import {
   SubscriptionBookingListProps,
   SubscriptionListResponse,
-  SubscriptionChangeCarProps,
   SubscriptionChangeCarInBookingProps,
-  SubscriptionExtendEndDateProps,
 } from 'services/web-bff/subscription.type'
 
 export const status = {
@@ -44,17 +40,6 @@ export const getList = async ({
 /**
  * This function allows operators to update a car with changing plate numbers.
  */
-export const changeCar = async ({
-  subscriptionId,
-  carId,
-}: SubscriptionChangeCarProps): Promise<boolean> => {
-  await BaseApi.patch(`/v1/subscriptions/${subscriptionId}/delivery/cars`, {
-    carId,
-  })
-
-  return true
-}
-
 export const changeCarInBooking = async ({
   bookingId,
   bookingDetailId,
@@ -70,32 +55,7 @@ export const changeCarInBooking = async ({
   return true
 }
 
-/**
- * This function allows operators to extend the end date of a subscription
- */
-export const extend = async ({
-  accessToken,
-  subscriptionId,
-  endDate,
-}: SubscriptionExtendEndDateProps): Promise<boolean> => {
-  await axios.patch(
-    `https://run.mocky.io/v3/bf06262b-712b-48de-9808-0b71c8c3958d`,
-    {
-      subscriptionId,
-      endDate,
-    },
-    {
-      headers: {
-        authorization: `Bearer ${accessToken}`,
-      },
-    }
-  )
-
-  return true
-}
-
 export default {
   getList,
-  changeCar,
   changeCarInBooking,
 }
