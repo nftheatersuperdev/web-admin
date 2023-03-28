1b27866500cd55f9d228fe5b9747e31e8e4e33aa
suppachai.n.evme <suppachai.n@evme.io>
2023-03-28 11:41:35 +0700

    feat(EVME-3351): re design list car-availability

--- src/pages/CarAvailability/index.tsx
+++ src/pages/CarAvailability/index.tsx
@@ -1,10 +1,10 @@
-import { useState, useEffect, Fragment } from 'react'
+import { useState, useEffect } from 'react'
 import {
   GridColDef,
   GridPageChangeParams,
-  GridFilterModel,
-  GridFilterItem,
-  GridSortModel,
+  GridToolbarColumnsButton,
+  GridToolbarContainer,
+  GridToolbarDensitySelector,
 } from '@material-ui/data-grid'
 import { useTranslation } from 'react-i18next'
 import {
@@ -12,23 +12,41 @@ import {
   DEFAULT_DATE_FORMAT_BFF,
   getStringFilterOperators,
   getEqualFilterOperators,
-  FieldComparisons,
   FieldKeyOparators,
+  validateKeywordText,
 } from 'utils'
 import config from 'config'
 import dayjs from 'dayjs'
 import dayjsUtc from 'dayjs/plugin/utc'
 import dayjsTimezone from 'dayjs/plugin/timezone'
-import { Box, Button, Card } from '@material-ui/core'
+import {
+  Box,
+  Button,
+  Card,
+  FormControl,
+  Grid,
+  InputAdornment,
+  makeStyles,
+  MenuItem,
+  Select,
+  TextField,
+} from '@material-ui/core'
 import { useQuery } from 'react-query'
+import { Search as SearchIcon } from '@material-ui/icons'
+import { useFormik } from 'formik'
+import Pagination from '@mui/lab/Pagination'
 import { getAvailableListBFF } from 'services/web-bff/car'
 import { CarAvailableListFilterRequest } from 'services/web-bff/car.type'
 import DatePicker from 'components/DatePicker'
-import PageToolbar from 'layout/PageToolbar'
-import { SortDirection, SubOrder } from 'services/evme.types'
 import { Page } from 'layout/LayoutRoute'
 import DataGridLocale from 'components/DataGridLocale'
-import { getVisibilityColumns, setVisibilityColumns, VisibilityColumns } from './utils'
+import PageTitle, { PageBreadcrumbs } from 'components/PageTitle'
+import {
+  getVisibilityColumns,
+  setVisibilityColumns,
+  VisibilityColumns,
+  getSearchTypeList,
+} from './utils'
 
 dayjs.extend(dayjsUtc)
 dayjs.extend(dayjsTimezone)
@@ -36,13 +54,45 @@ dayjs.extend(dayjsTimezone)
 const initSelectedFromDate = dayjs().tz(config.timezone).startOf('day').toDate()
 const initSelectedToDate = dayjs().tz(config.timezone).endOf('day').toDate()
 
+const useStyles = makeStyles(() => ({
+  searchBar: {
+    margin: '20px 0 0 0',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  fullWidth: {
+    width: '100%',
+  },
+  filter: {
+    height: '90px',
+  },
+  buttonWithoutShadow: {
+    display: 'inline-flexbox',
+    boxShadow: 'none',
+    padding: '16px 20px',
+  },
+  paginationContrainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '20px',
+  },
+  inlineElement: {
+    display: 'inline-flex',
+  },
+}))
+
 export default function Car(): JSX.Element {
   const { t } = useTranslation()
+  const classes = useStyles()
   const [selectedFromDate, setSelectedFromDate] = useState(initSelectedFromDate)
   const [selectedToDate, setSelectedToDate] = useState(initSelectedToDate)
   const [pageSize, setPageSize] = useState(config.tableRowsDefaultPageSize)
   const [page, setPage] = useState(0)
-
+  const searchTypeList = getSearchTypeList(t)
+  const [filterPlate, setFilterPlate] = useState<string>('')
+  const [filterPlateError, setFilterPlateError] = useState<string>('')
   const generateFilterDates = () => {
     return {
       startDate: dayjs(selectedFromDate).startOf('day').format(DEFAULT_DATE_FORMAT_BFF),
@@ -50,7 +100,6 @@ export default function Car(): JSX.Element {
     }
   }
 
-  const [sort, setSort] = useState<SubOrder>({})
   const [filter, setFilter] = useState<CarAvailableListFilterRequest>(generateFilterDates())
 
   const {
@@ -60,7 +109,6 @@ export default function Car(): JSX.Element {
   } = useQuery('availability-cars', () =>
     getAvailableListBFF({
       filter,
-      sort,
       page,
       size: pageSize,
     })
@@ -76,37 +124,6 @@ export default function Car(): JSX.Element {
 
   const handlePageSizeChange = (params: GridPageChangeParams) => {
     setPageSize(params.pageSize)
-  }
-
-  const handleFilterChange = (params: GridFilterModel) => {
-    setFilter(
-      params.items.reduce((filterItem, { columnField, operatorValue, value }: GridFilterItem) => {
-        const isId = columnField === 'id'
-
-        let keyOfValue = ''
-        if (value) {
-          if (isId) {
-            keyOfValue = 'carId'
-          } else {
-            switch (operatorValue) {
-              case FieldComparisons.equals:
-                keyOfValue = `${columnField}${FieldKeyOparators.equals}`
-                break
-              case FieldComparisons.contains:
-                keyOfValue = `${columnField}${FieldKeyOparators.contains}`
-                break
-            }
-          }
-          filterItem = { [keyOfValue]: value }
-        }
-        filterItem = {
-          ...filterItem,
-          ...generateFilterDates(),
-        }
-        return filterItem
-      }, {} as CarAvailableListFilterRequest)
-    )
-    setPage(0)
   }
 
   // eslint-disable-next-line  @typescript-eslint/no-explicit-any
@@ -126,26 +143,6 @@ export default function Car(): JSX.Element {
     visibilityColumns[params.field] = params.isVisible
 
     setVisibilityColumns(visibilityColumns)
-  }
-
-  const handleSortChange = (params: GridSortModel) => {
-    if (params?.length > 0 && !isFetching) {
-      const { field: refField, sort } = params[0]
-
-      const order: SubOrder = {
-        [refField]: sort?.toLocaleLowerCase() === 'asc' ? SortDirection.Asc : SortDirection.Desc,
-      }
-
-      setSort(order)
-      refetch()
-    }
-  }
-
-  const handleClickSearchButton = () => {
-    setFilter({
-      ...filter,
-      ...generateFilterDates(),
-    })
   }
 
   const rowCount = carData?.data?.pagination?.totalRecords ?? 0
@@ -236,51 +233,164 @@ export default function Car(): JSX.Element {
       flex: 1,
     },
   ]
+  const formik = useFormik({
+    initialValues: {
+      input: '',
+      searchType: '',
+    },
+    enableReinitialize: true,
 
+    onSubmit: (value) => {
+      const isId = value.searchType === 'id'
+      let keyOfValue = ''
+      let updateObj
+
+      if (value.searchType === '') {
+        filter.carId = undefined
+        filter.plateNumberContain = undefined
+        filter.plateNumberEqual = undefined
+        updateObj = {
+          ...filter,
+          ...generateFilterDates(),
+        } as CarAvailableListFilterRequest
+      } else {
+        if (isId) {
+          keyOfValue = 'carId'
+        } else {
+          keyOfValue = `${value.searchType}${FieldKeyOparators.contains}`
+        }
+        updateObj = {
+          [keyOfValue]: filterPlate,
+          ...generateFilterDates(),
+        } as CarAvailableListFilterRequest
+      }
+      setFilter(updateObj)
+      setPage(0)
+    },
+  })
+  const conditionConfigs = {
+    minimumToFilterPlateNumber: 2,
+  }
+  const handleOnPlateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target
+    const isKeywordAccepted = validateKeywordText(value)
+    setFilterPlate(value)
+    setFilterPlateError('')
+    if (isKeywordAccepted && value.length >= conditionConfigs.minimumToFilterPlateNumber) {
+      setFilterPlate(value)
+    } else if (value !== '') {
+      setFilterPlateError(t('carActivity.plateNumber.errors.invalidFormat'))
+    } else {
+      setFilterPlate('')
+    }
+  }
+  const customToolbar = () => (
+    <GridToolbarContainer>
+      <GridToolbarColumnsButton />
+      <GridToolbarDensitySelector />
+    </GridToolbarContainer>
+  )
+  const breadcrumbs: PageBreadcrumbs[] = [
+    {
+      text: t('sidebar.vehicleManagement'),
+      link: '/',
+    },
+    {
+      text: t('sidebar.carAvailability'),
+      link: '/car-availability',
+    },
+  ]
   return (
     <Page>
-      <PageToolbar>
-        <Fragment>
-          <DatePicker
-            label={t('carAvailability.selectedFromDate')}
-            id="car_availability__startdate_input"
-            KeyboardButtonProps={{
-              id: 'car_availability__startdate_icon',
-            }}
-            name="selectedFromDate"
-            format={DEFAULT_DATE_FORMAT}
-            value={selectedFromDate}
-            onChange={(date) => {
-              date && setSelectedFromDate(date.toDate())
-            }}
-          />
-          <DatePicker
-            label={t('carAvailability.selectedToDate')}
-            id="car_availability__enddate_input"
-            KeyboardButtonProps={{
-              id: 'car_availability__enddate_icon',
-            }}
-            name="selectedToDate"
-            format={DEFAULT_DATE_FORMAT}
-            value={selectedToDate}
-            onChange={(date) => {
-              date && setSelectedToDate(date.toDate())
-            }}
-          />
-          <Box display="flex" alignItems="center">
-            <Button
-              id="car_availability__search_btn"
-              color="primary"
-              variant="contained"
-              onClick={handleClickSearchButton}
-            >
-              {t('carAvailability.search')}
-            </Button>
-          </Box>
-        </Fragment>
-      </PageToolbar>
-
+      <PageTitle title="Car Availability" breadcrumbs={breadcrumbs} />
       <Card>
+        <Grid className={classes.searchBar} container spacing={3}>
+          <Grid item className={[classes.filter].join(' ')} xs={2}>
+            <TextField
+              fullWidth
+              select
+              label="Select Search"
+              variant="outlined"
+              id="document"
+              value={formik.values.searchType}
+              onChange={(event) => {
+                formik.setFieldValue('searchType', event.target.value)
+                setFilterPlate('')
+              }}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              {searchTypeList?.map((option) => (
+                <MenuItem key={option.key} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
+          <Grid item className={[classes.filter].join(' ')} xs={3}>
+            <TextField
+              disabled={formik.values.searchType === ''}
+              className={classes.fullWidth}
+              error={!!filterPlateError}
+              helperText={filterPlateError}
+              variant="outlined"
+              placeholder={t('carActivity.plateNumber.placeholder')}
+              value={filterPlate}
+              onChange={handleOnPlateChange}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Grid>
+          <Grid item className={[classes.filter].join(' ')} xs={2}>
+            <DatePicker
+              label={t('carAvailability.selectedFromDate')}
+              id="car_availability__startdate_input"
+              KeyboardButtonProps={{
+                id: 'car_availability__startdate_icon',
+              }}
+              name="selectedFromDate"
+              format={DEFAULT_DATE_FORMAT}
+              value={selectedFromDate}
+              onChange={(date) => {
+                date && setSelectedFromDate(date.toDate())
+              }}
+            />
+          </Grid>
+          <Grid item className={[classes.filter].join(' ')} xs={2}>
+            <DatePicker
+              label={t('carAvailability.selectedToDate')}
+              id="car_availability__enddate_input"
+              KeyboardButtonProps={{
+                id: 'car_availability__enddate_icon',
+              }}
+              name="selectedToDate"
+              format={DEFAULT_DATE_FORMAT}
+              value={selectedToDate}
+              onChange={(date) => {
+                date && setSelectedToDate(date.toDate())
+              }}
+            />
+          </Grid>
+          <Grid item className={[classes.filter].join(' ')} xs={3}>
+            <Box display="flex" alignItems="center">
+              <Button
+                id="car_availability__search_btn"
+                className={classes.buttonWithoutShadow}
+                color="primary"
+                variant="contained"
+                onClick={() => formik.handleSubmit()}
+              >
+                {t('carAvailability.search')}
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
         <DataGridLocale
           autoHeight
           pagination
@@ -292,13 +402,41 @@ export default function Car(): JSX.Element {
           onPageChange={setPage}
           rows={rows}
           columns={columns}
-          filterMode="server"
-          onFilterModelChange={handleFilterChange}
           onColumnVisibilityChange={onColumnVisibilityChange}
           sortingMode="server"
-          onSortModelChange={handleSortChange}
           loading={isFetching}
+          customToolbar={customToolbar}
+          hideFooter
         />
+      </Card>
+      <Card>
+        <div className={classes.paginationContrainer}>
+          Rows per page:&nbsp;
+          <FormControl className={classes.inlineElement}>
+            <Select
+              value={carData?.data?.pagination?.size || pageSize}
+              defaultValue={carData?.data?.pagination?.size || pageSize}
+              onChange={(event: React.ChangeEvent<{ name?: string; value: unknown }>) => {
+                setPage(1)
+                setPageSize(event.target.value as number)
+              }}
+            >
+              <MenuItem value={10}>10</MenuItem>
+              <MenuItem value={20}>20</MenuItem>
+              <MenuItem value={50}>50</MenuItem>
+            </Select>
+          </FormControl>
+          <Pagination
+            count={carData?.data?.pagination?.totalPage}
+            page={carData?.data?.pagination?.page || page}
+            defaultPage={carData?.data?.pagination?.page || page}
+            variant="text"
+            color="primary"
+            onChange={(_event: React.ChangeEvent<unknown>, value: number) => {
+              setPage(value - 1)
+            }}
+          />
+        </div>
       </Card>
     </Page>
   )
