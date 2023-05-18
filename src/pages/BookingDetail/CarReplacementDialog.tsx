8aa4fc7dbbea7ca07ecb17de73cb4a1d3dac92f9
Krissada Boontrigratn <krissada@appsynth.net>
2023-05-18 13:29:28 +0700

    fix: remove unused code

--- src/pages/BookingDetail/CarReplacementDialog.tsx
+++ src/pages/BookingDetail/CarReplacementDialog.tsx
@@ -84,7 +84,6 @@ export default function CarReplacementDialog({
     bookingId,
     rentDetail: { bookingDetailId },
     carActivities,
-    displayStatus,
     startDate,
     endDate,
     displayStatus,
