66d21249aa4e659a46fa64525e7f9173caf8010d
Veerapat Prechadech <veerapat.p@evme.io>
2023-06-08 16:36:34 +0700

    feat: add id

--- src/pages/ConsentsLog/index.tsx
+++ src/pages/ConsentsLog/index.tsx
@@ -175,38 +175,38 @@ export default function ConsentsLog(): JSX.Element {
       consentList.data.agreements.map((agreement) => {
         // Build Table Body
         return (
-          <TableRow key={agreement.id}>
-            <TableCell>
+          <TableRow id={`consent_log__id-${agreement.id}`} key={agreement.id}>
+            <TableCell id="consent_log__firstName">
               <DataWrapper>
                 <TextSmallLineClamp>{agreement.customer.firstName}</TextSmallLineClamp>
               </DataWrapper>
             </TableCell>
-            <TableCell>
+            <TableCell id="consent_log__lastName">
               <DataWrapper>
                 <TextSmallLineClamp>{agreement.customer.lastName}</TextSmallLineClamp>
               </DataWrapper>
             </TableCell>
-            <TableCell>
+            <TableCell id="consent_log__email">
               <DataWrapper>
                 <TextSmallLineClamp>{agreement.customer.email}</TextSmallLineClamp>
               </DataWrapper>
             </TableCell>
-            <TableCell>
+            <TableCell id="consent_log__phoneNumber">
               <DataWrapper>
                 <TextSmallLineClamp>
                   {convertPhoneNumber(agreement.customer.phoneNumber)}
                 </TextSmallLineClamp>
               </DataWrapper>
             </TableCell>
-            <TableCell>
+            <TableCell id="consent_log__documentName">
               <DataWrapper>
                 <TextLineClamp>{agreement.documentContent.nameEn}</TextLineClamp>
               </DataWrapper>
             </TableCell>
-            <TableCell width={50}>
+            <TableCell id="consent_log__documentVersion" width={50}>
               <DataWrapper>{agreement.documentContent.version}</DataWrapper>
             </TableCell>
-            <TableCell width={50}>
+            <TableCell id="consent_log__documentStatus" width={50}>
               {!agreement.isAccepted ? (
                 <Chip
                   size="small"
@@ -221,7 +221,7 @@ export default function ConsentsLog(): JSX.Element {
                 />
               )}
             </TableCell>
-            <TableCell>
+            <TableCell id="consent_log__updatedDate">
               <DataWrapper>
                 <TextLineClamp>
                   {formaDateStringWithPattern(
