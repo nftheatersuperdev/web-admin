b440c9b9711076d836cf46f8f8eb89fe89625c02
Krissada Boontrigratn <krissada@appsynth.net>
2023-06-19 02:13:07 +0700

    feat: improve state of value in multiple search field component

--- src/components/MultipleSearchField.tsx
+++ src/components/MultipleSearchField.tsx
@@ -1,16 +1,20 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { useState, ChangeEvent, useRef } from 'react'
-import { Autocomplete, Grid, TextField, InputAdornment, IconButton } from '@mui/material'
+import { Autocomplete, Grid, TextField, InputAdornment, IconButton, Button } from '@mui/material'
 import { Search as SearchIcon } from '@mui/icons-material'
 import { useTranslation } from 'react-i18next'
 import { DEFAULT_DATE_FORMAT_MONTH_TEXT } from 'utils'
 import styled from 'styled-components'
+import dayjs from 'dayjs'
 import DatePicker from 'components/DatePicker'
 
 export const SearchDatePicker = styled(DatePicker)`
   .MuiInputBase-root {
-    height: 51.69px;
+    height: 51px;
   }
+`
+const SearchSubmitButton = styled(Button)`
+  height: 51px;
 `
 
 export interface SelectOption {
@@ -30,7 +34,8 @@ export interface SearchField {
 interface MultipleSearchFieldProps {
   id: string
   fields: SearchField[]
-  onInputChanged: (inputId: string | null, inputValue: string | undefined | null) => void
+  onSubmit: (inputId: string | undefined | null, inputValue: string | undefined | null) => void
+  spacing?: number
 }
 
 interface RenderTextFieldProps {
@@ -49,7 +54,8 @@ interface RenderDatePickerFieldProps {
 export default function MultipleSearchField({
   id,
   fields,
-  onInputChanged,
+  spacing,
+  onSubmit,
 }: MultipleSearchFieldProps): JSX.Element {
   const { t } = useTranslation()
 
@@ -57,12 +63,18 @@ export default function MultipleSearchField({
   const [selectedOption, setSelectedOption] = useState<SelectOption | null | undefined>(null)
   const [stateValue, setStateValue] = useState<string>()
 
+  const isDisabledSearchButton = !stateValue
+
   const searchOptions: SelectOption[] = fields.map((field) => {
     return {
       label: field.optionLabel,
       value: field.optionId,
     }
   })
+
+  function handleOnSubmit() {
+    onSubmit(selectedOption?.value, stateValue)
+  }
 
   function renderTextField({ fieldId, placeholder, defaultValue, disabled }: RenderTextFieldProps) {
     return (
@@ -75,16 +87,17 @@ export default function MultipleSearchField({
         onChange={(event: ChangeEvent<HTMLInputElement>, value?: string) => {
           const { value: eventValue } = event.target
           const fieldValue = value ? value : eventValue
-          onInputChanged(fieldId, fieldValue)
+          setStateValue(() => fieldValue)
         }}
         placeholder={placeholder}
         disabled={disabled}
+        value={stateValue}
         defaultValue={defaultValue}
         InputProps={{
           startAdornment: (
             <InputAdornment position="start">
               <IconButton disabled={disabled}>
-                <SearchIcon color="action" />
+                <SearchIcon color={disabled ? 'disabled' : 'action'} />
               </IconButton>
             </InputAdornment>
           ),
@@ -108,13 +121,11 @@ export default function MultipleSearchField({
         inputVariant="outlined"
         onChange={(date) => {
           if (date) {
-            const dateToString = date.toISOString()
+            const dateToString = date.startOf('day').toISOString()
             setStateValue(dateToString)
-            onInputChanged(fieldId, dateToString)
             return
           }
           setStateValue('')
-          onInputChanged(fieldId, null)
         }}
       />
     )
@@ -127,9 +138,6 @@ export default function MultipleSearchField({
 
     if (textInputElement.current) {
       textInputElement.current.value = ''
-    }
-    if (!selectedOption && stateValue) {
-      setStateValue('')
     }
 
     if (!searchField || !selectedOption) {
@@ -151,6 +159,9 @@ export default function MultipleSearchField({
         })
       }
       case 'datepicker': {
+        if (!stateValue) {
+          setStateValue(() => dayjs().startOf('day').toISOString())
+        }
         return renderDatePickerField({
           fieldId: optionId,
           fieldLable: optionLabel,
@@ -161,8 +172,8 @@ export default function MultipleSearchField({
   }
 
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12} sm={6}>
+    <Grid container spacing={spacing}>
+      <Grid item xs={12} sm={6} md={5}>
         <Autocomplete
           autoHighlight
           id="search_select_list"
@@ -176,17 +187,29 @@ export default function MultipleSearchField({
           }
           value={selectedOption || null}
           onChange={(_e, value) => {
+            setStateValue(() => '')
             if (value) {
-              setSelectedOption(value)
+              setSelectedOption(() => value)
               return
             }
-            setSelectedOption(null)
-            onInputChanged(null, null)
+            setSelectedOption(() => null)
           }}
         />
       </Grid>
-      <Grid item xs={12} sm={6}>
+      <Grid item xs={12} sm={6} md={5}>
         {renderSearchInputField()}
+      </Grid>
+      <Grid item xs={12} sm={6} md={2}>
+        <SearchSubmitButton
+          fullWidth
+          id="user_group_search_button"
+          variant="contained"
+          size="large"
+          disabled={isDisabledSearchButton}
+          onClick={handleOnSubmit}
+        >
+          {t('carAvailability.searchBtn').toUpperCase()}
+        </SearchSubmitButton>
       </Grid>
     </Grid>
   )
