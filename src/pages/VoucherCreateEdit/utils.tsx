abfcb8697abc69c3ec95f8acea729e88b476d63c
Krissada Boontrigratn <krissada@appsynth.net>
2023-06-20 17:41:42 +0700

    feat(EVME-3356): create and update on voucher is done

---
+++ src/pages/VoucherCreateEdit/utils.tsx
@@ -0,0 +1,37 @@
+import { FormEvent, KeyboardEvent } from 'react'
+
+export const handleValidateNumericKeyPress = (event: KeyboardEvent): void => {
+  const allowCharacters = /[0-9]/
+  if (!allowCharacters.test(event.key)) {
+    event.preventDefault()
+  }
+}
+
+export const handleValidatePercentageValue = (event: FormEvent, maximum = 100): void => {
+  const target = event.target as HTMLInputElement
+  target.value = target.value.toUpperCase().replace(/\s/g, '')
+  if (Number(target.value) > maximum) {
+    target.value = String(maximum)
+  }
+}
+
+export const handleValidateCodeValue = (event: FormEvent): void => {
+  const target = event.target as HTMLInputElement
+  target.value = target.value.toUpperCase().replace(/\s/g, '')
+}
+
+export const handleValidateCodeKeyPress = (event: React.KeyboardEvent): void => {
+  const allowCharacters = /[a-zA-Z0-9]/
+  if (!allowCharacters.test(event.key)) {
+    event.preventDefault()
+  }
+}
+
+export const handleDisableEvent = (event: FormEvent): void => {
+  event.preventDefault()
+}
+
+export const selectOptions = {
+  ALL: 'all',
+  SELECT: 'select',
+}
