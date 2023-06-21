988bfbcb32e46f49a98e9ec8da990e772c13864b
Krissada Boontrigratn <krissada@appsynth.net>
2023-06-21 10:49:55 +0700

    fix: qa comments

--- src/components/Paginate.tsx
+++ src/components/Paginate.tsx
@@ -13,8 +13,15 @@ const PaginateContainer = styled.div`
   justify-content: flex-end;
   padding: 20px;
 `
+const FormWrapper = styled.div`
+  padding: 0 10px;
+`
 const FormControlInline = styled(FormControl)`
   display: inline-flex !important;
+  background: none;
+`
+const TextWrapper = styled.span`
+  padding: 4px 0;
 `
 
 interface PaginateProps {
@@ -42,24 +49,28 @@ export default function Paginate({
 
   return (
     <PaginateContainer>
-      {t('pagination.rowsPerPage')}:{' '}
-      <FormControlInline variant="standard">
-        <Select
-          value={pagination.size || pageSize}
-          defaultValue={pagination.size || pageSize}
-          onChange={(event) => {
-            setPage(1)
-            setPageSize(event.target.value as number)
-          }}
-        >
-          {config.tableRowsPerPageOptions?.map((option) => (
-            <MenuItem key={option} value={option}>
-              {option}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControlInline>{' '}
-      {pagination.page} {t('booking.of')} {pagination.totalPage}
+      <TextWrapper>{t('pagination.rowsPerPage')}: </TextWrapper>
+      <FormWrapper>
+        <FormControlInline variant="standard">
+          <Select
+            value={pagination.size || pageSize}
+            defaultValue={pagination.size || pageSize}
+            onChange={(event) => {
+              setPage(1)
+              setPageSize(event.target.value as number)
+            }}
+          >
+            {config.tableRowsPerPageOptions?.map((option) => (
+              <MenuItem key={option} value={option}>
+                {option}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControlInline>
+      </FormWrapper>
+      <TextWrapper>
+        {pagination.page} {t('booking.of')} {pagination.totalPage}
+      </TextWrapper>
       <Pagination
         count={pagination.totalPage}
         page={pagination.page || page}
